d0f4adc5cffa19dd0138eda0aa35dea6a368eddc
djibril soukouna <djib_cisse_9@hotmail.com>
2023-07-13 18:48:07 +0200

    first front

--- hackatweet-front/components/Home.js
+++ hackatweet-front/components/Home.js
@@ -1,12 +1,21 @@
 import styles from '../styles/Home.module.css';
 
+
 function Home() {
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.imageGauche}>
+        {/* <img src="Twitter.jpg" /> */}
+      </div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        
+        <img src="logo twitter.png" />
+          <h1 className={styles.h1}>See what's happening</h1>
+          <h3>Join Hackatweet today.</h3>
+          <button clas>Sign up</button>
+          <p>Already have an account?</p>
+          <button>Sign in</button>
+        
       </main>
     </div>
   );
